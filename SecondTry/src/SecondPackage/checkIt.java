10293c2030a1ad25d2609568c5ae73f819571d52
murod <murod@DESKTOP-RCOQVE2>
2021-03-12 12:02:01 +0500

    re-saved

--- SecondTry/src/SecondPackage/checkIt.java
+++ SecondTry/src/SecondPackage/checkIt.java
@@ -9,7 +9,7 @@ public class checkIt {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\murod\\\\Desktop\\\\JAVA\\\\ChromeDriver\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\murod\\Desktop\\JAVA\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://facebook.com");
